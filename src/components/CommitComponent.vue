aac05f0ee7f3ca69370847efcee3d6ea12bd4ba6
Anna Chernysheva <dx8565@gmail.com>
2023-12-20 10:31:09 +0500

    chore(components): add text section Commit

---
+++ src/components/CommitComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Commit + amend</template>
+
+Commit + amend
+  </WelcomeItem>
+</template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,9 +14,7 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Commit</template>
 
-<!--    As an independent project, Vue relies on community backing for its sustainability. You can help-->
-<!--    us by-->
-<!--    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.-->
+Commit + amend
   </WelcomeItem>
 
   <WelcomeItem>
